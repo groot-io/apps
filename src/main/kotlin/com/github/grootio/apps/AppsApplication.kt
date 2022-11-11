71a6c59c9d4b20136a914a24430c9ec44da51c0e
Matheus Cruz <matheus.mcruz@mercadolivre.com>
2022-11-10 22:37:19 -0300

    chore: add spring project

---
+++ src/main/kotlin/com/github/grootio/apps/AppsApplication.kt
@@ -0,0 +1,11 @@
+package com.github.grootio.apps
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class AppsApplication
+
+fun main(args: Array<String>) {
+	runApplication<AppsApplication>(*args)
+}
